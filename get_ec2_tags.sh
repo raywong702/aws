95de518a52894c4cd2a790ce3494c015d815f515
Raymond <r.wong@live.com>
2017-06-03 15:49:26 -0400

    print name tag

--- get_ec2_tags.sh
+++ get_ec2_tags.sh
@@ -3,3 +3,6 @@
 instanceid=`curl -s http://169.254.169.254/latest/meta-data/instance-id`
 region=`curl -s http://instance-data/latest/meta-data/placement/availability-zone | sed 's/.$//'`
 name=`aws ec2 describe-tags --region $region --filter "Name=resource-id,Values=$instanceid" "Name=key,Values=Name" --output=text | cut -f5`
+echo $name
+
+exit 0
