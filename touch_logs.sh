fc44381419cedf02f3ef4d47511c22ee6ff4a4ad
Raymond <r.wong@live.com>
2017-06-05 19:24:17 -0400

    chmod and touch_logs

---
+++ touch_logs.sh
@@ -0,0 +1,15 @@
+a=201706042359
+b=201706032359
+c=201706022359
+
+d=access_log.1240921107
+e=access_log.3408210027
+f=access_log.3423032082
+
+for i in `cat hosts | awk '{print $2}'`
+do
+    ssh -q vzwuser@$i "cd /log; touch error_log; touch access_log; \
+        touch -t $a $d; touch -t $b $e; touch -t $c $f"
+done
+
+exit 0
