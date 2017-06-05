140cba39b281943a9780b74b45431877b6f6cd60
Raymond <r.wong@live.com>
2017-06-05 19:34:10 -0400

    ignore hostkeys

--- copy_assignment_file.sh
+++ copy_assignment_file.sh
@@ -1,7 +1,7 @@
 #!/usr/local/bin/bash
 for i in `cat hosts | awk '{print $2}'`
 do
-    scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null asignment.txt vzwuser@$i:/app
+    scp -o "StrictHostKeyChecking=no" asignment.txt vzwuser@$i:/app
 done
 
 exit 0
